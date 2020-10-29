bb9995b28f1cee3cf4ffd198060e01361f092a78
Matt Mayes <mattamayes@gmail.com>
2020-10-29 17:32:56 -0500

    App settings update.

--- config/app.php
+++ config/app.php
@@ -290,6 +290,7 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             'timezone' => 'UTC',
+            'url' => env('DATABASE_URL', null),
 
             /**
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
